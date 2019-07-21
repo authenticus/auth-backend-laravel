39c8805e7a89dc9c22da0dc3249275fe7be1f942
sustained <sustained.dissonance@gmail.com>
2019-07-21 13:49:41 +0100

    Don't remove explanatory comment.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Http\Request;
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
 Route::group([
     'prefix' => 'auth'
 ], function () {
