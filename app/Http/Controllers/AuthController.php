7fef7d336fc07b2d41ff34b06447078261c110d6
sustained <sustained.dissonance@gmail.com>
2019-07-21 03:13:03 +0100

    Fix 1x invalid token reference.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -57,7 +57,7 @@ class AuthController extends Controller
         if ($request->remember_me)
             $token->token->expires_at = Carbon::now()->addWeeks(1);
 
-        $token->save();
+        $token->token->save();
 
         return response()->json([
             'access_token' => $token->token->accessToken,
