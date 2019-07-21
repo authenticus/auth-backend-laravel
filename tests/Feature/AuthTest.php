5251e766ae3bc8b6a0a17034a9043e8695fe605b
sustained <sustained.dissonance@gmail.com>
2019-07-21 15:56:23 +0100

    wip: Add AuthTest.

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        \Artisan::call('migrate');
+        \Artisan::call('passport:install');
+    }
+
+    public function test_that_registration_attempts_are_validated()
+    {
+        $this
+            ->postJson('/api/auth/register')
+            ->assertJsonValidationErrors(['name', 'email', 'password']);
+
+        $this
+            ->postJson('/api/auth/register', [
+                'email' => 'test'
+            ])
+            ->assertJsonFragment(['The email must be a valid email address.']);
+
+        $this
+            ->postJson('/api/auth/register', [
+                'password' => 'hunter2'
+            ])
+            ->assertJsonFragment(['The password confirmation does not match.']);
+    }
+
+    public function test_that_successful_registration_attempts_returns_valid_tokens()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this
+            ->postJson('/api/auth/register', [
+                'name' => 'Test',
+                'email' => 'test@test.com',
+                'password' => 'hunter2',
+                'password_confirmation' => 'hunter2'
+            ])
+            ->assertJsonFragment(['type' => 'register_success'])
+            ->assertJsonStructure([
+                'access_token',
+                'expires_at',
+                'token_type',
+
+                'type',
+                'message'
+            ]);
+
+        $this
+            ->assertNotNull($response->json()['access_token']) // FIXME: Our access tokens are currently always null.
+            ->assertDatabaseHas('users', ['name' => 'Test'])
+            ->assertDatabaseHas('oauth_access_tokens', ['user_id' => 1]);
+    }
+}
